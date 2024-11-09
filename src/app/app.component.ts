8dd380e6122864248b756886213fe0ef114edd3c
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2024-11-09 23:24:39 +0200

    create component for view data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import type { InvestmentInput } from './investment-input.model';
+import type { InvestmentResults } from './investment-results.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  resultsData: InvestmentResults[] = new Array<InvestmentResults>();
+  
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      } as InvestmentResults);
+    }
+    this.resultsData = annualData;
+  }
+}
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import type { InvestmentResults } from '../investment-results.model';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  results = input<InvestmentResults[]>();
+}
